d8e659de0218292a25c9177744b8482239c29a14
Mitra Omrani <mitraomrani.2001@gmail.com>
2022-01-08 09:09:51 +0330

    Finish delivery Part

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -1,12 +1,9 @@
 #include<bits/stdc++.h>
+#include <stdlib.h>
 using namespace std;
 using ll=long long;
 vector<string> Rs_map;
 pair<ll,ll>kitchen_plc;
-map<pair<ll,ll>,int> visited;
-map<pair<ll,ll>,pair<ll,ll>> path;
-queue<pair<ll,ll>> q;
-set<pair<ll,ll>> final_path;
 int length=0;
 struct customer{
     string name;
@@ -14,8 +11,10 @@ struct customer{
     ll prepartion_time;
     ll eating_time;
 };
+
 void print_path(ll i_fnd,ll j_fnd,map<pair<ll,ll>,pair<ll,ll>> path){
     ll i=i_fnd,j=j_fnd,prv;
+    set<pair<ll,ll>> final_path;
     while(1){
         final_path.insert({i,j});
         prv=i;
@@ -24,22 +23,26 @@ void print_path(ll i_fnd,ll j_fnd,map<pair<ll,ll>,pair<ll,ll>> path){
         if(i==kitchen_plc.first && j==kitchen_plc.second) break;
     }
     final_path.insert({kitchen_plc.first,kitchen_plc.second});
-    cout<<final_path.size()<<endl;
-    //HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    ll k=13;
-    // printing the map and coloring the nodes that are included in the shortest path
     for(ll i=0;i<length;i++){
         for(ll j=0;j<Rs_map[i].size();j++){
-             if(final_path.find({i,j})!=final_path.end()) cout<<' ';
-        //         SetConsoleTextAttribute(hConsole, k),cout<<k;
+             if(final_path.find({i,j})!=final_path.end())    
+             {
+                 printf("\x1B[35m");
+                 cout<<Rs_map[i][j];
+                 printf("\033[0m");
+             }
             else cout<<Rs_map[i][j];
         }
         cout<<"\n";
     }
 }
+
 void find_shortest_path(char search){
     bool found=0;
     ll i_fnd,j_fnd;
+    map<pair<ll,ll>,int> visited;
+    map<pair<ll,ll>,pair<ll,ll>> path;
+    queue<pair<ll,ll>> q;
     visited[{kitchen_plc.first,kitchen_plc.second}]=1;
     // applying bfs for shortest path
     q.push({kitchen_plc.first,kitchen_plc.second});
@@ -67,36 +70,38 @@ void find_shortest_path(char search){
         if(found) break;
     }
     if(!found) cout<<"CHE KHAKI BE SARAM KONAM GHAZAM YAKH KARD\n";
-    else {
-        cout<<"sdds"<<endl;
-        print_path(i_fnd,j_fnd,path);
-     }
+    else  print_path(i_fnd,j_fnd,path);
 }
-int main(){
-    string input_str,customer_detail;
-    // getting the map
-    while (cin>>input_str){
-        Rs_map.push_back(input_str);
-       if ( std::string::npos != input_str.find('$')){
-           kitchen_plc.first=length;
-           kitchen_plc.second=input_str.find('$');
-       }
-    length++;
-    if(length==10) break;
+
+void deliverFood(){
+       // getting the map
+       string input_str;
+        while (cin>>input_str){
+            Rs_map.push_back(input_str);
+        if ( std::string::npos != input_str.find('$')){
+            kitchen_plc.first=length;
+            kitchen_plc.second=input_str.find('$');
+        }
+        length++;
+        if(length==10) break;
+        }
+        find_shortest_path('b');
     }
-    find_shortest_path('d');
+int main(){
+    string customer_detail;
+    deliverFood();
     // getting constome's data
     vector<customer> costomer_info;
-    // cin.ignore();
-    // while(getline(cin,customer_detail)){
-    //     stringstream ss(customer_detail);
-    //     customer x;
-    //     string word;
-    //         ss>>word; x.name=word;
-    //         ss>>word; x.ordered_food=word;
-    //         ss>>word; x.prepartion_time=stoi(word);
-    //         ss>>word; x.eating_time=stoi(word);
-    //         costomer_info.push_back(x);
-    // }
+    cin.ignore();
+    while(getline(cin,customer_detail)){
+        stringstream ss(customer_detail);
+        customer x;
+        string word;
+            ss>>word; x.name=word;
+            ss>>word; x.ordered_food=word;
+            ss>>word; x.prepartion_time=stoi(word);
+            ss>>word; x.eating_time=stoi(word);
+            costomer_info.push_back(x);
+    }
     return 0;
 }
