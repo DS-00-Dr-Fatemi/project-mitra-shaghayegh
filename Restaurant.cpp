8f25f09cd4398edc564016af0071d95817cefeda
shaghayegh shahbazy <shsh6184@hotmail.com>
2022-01-14 16:22:08 +0330

    First version of Party

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -167,7 +167,350 @@ void shortest_distn_btwn_allNodes(){
     calculate_Min_distn();
     cout<<Tsp(1,0)<<endl;
 }
+/* Party part functions*/
+class Node
+{
+    //class of every node (every guests)
+public:
+    ll Turn;
+    string name;
+    Node* left, * right;
+    ll height;
+    Node(ll t, string n)
+    {
+        Turn = t;
+        name = n;
+        left = right = nullptr;
+        height = 1;
+    }
+};
+class AVLTree
+{
+public:
+    Node* root;
+    map<Node*, int>visited;
+    AVLTree()
+    {
+        root = nullptr;
+    }
+    ll height(Node* N)
+    {
+        //return height of node
+        if (N == NULL)
+            return 0;
+        return N->height;
+    }
+    Node* rightRotate(Node* y)
+    {
+        Node* x = y->left;
+        Node* T2 = x->right;
+        x->right = y;
+        y->left = T2;
+        y->height = max(height(y->left), height(y->right)) + 1;
+        x->height = max(height(x->left), height(x->right)) + 1;
+        return x;
+    }
+    Node* leftRotate(Node* x)
+    {
+        Node* y = x->right;
+        Node* T2 = y->left;
+        y->left = x;
+        x->right = T2;
+        x->height = max(height(x->left), height(x->right)) + 1;
+        y->height = max(height(y->left), height(y->right)) + 1;
+        return y;
+    }
+    ll getBalance(Node* N)
+    {
+        if (N == NULL)
+            return 0;
+        return height(N->left) - height(N->right);
+    }
+    Node* insert(Node* node, ll turn, string name)
+    {
+        //insert a new node(new guest)
+        if (node == NULL)
+            return(new Node(turn, name));
+        if (turn < node->Turn)
+            node->left = insert(node->left, turn, name);
+        else if (turn > node->Turn)
+            node->right = insert(node->right, turn, name);
+        else
+            return node;
+        node->height = 1 + max(height(node->left), height(node->right));
+        ll balance = getBalance(node);
+        if (balance > 1 && turn < node->left->Turn)
+            return rightRotate(node);
+        if (balance < -1 && turn > node->right->Turn)
+            return leftRotate(node);
+        if (balance > 1 && turn > node->left->Turn)
+        {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+        if (balance < -1 && turn < node->right->Turn)
+        {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+        return node;
+    }
+    Node* minValueNode(Node* node)
+    {
+        Node* current = node;
+        while (current->left != NULL)
+            current = current->left;
+        return current;
+    }
+    ll searchByName(string name)
+    {
+        //search in tree using dfs and return its turn
+        queue<Node*> q;
+        q.push(root);
+        visited[root] = 1;
+        while (!q.empty()) {
+            Node* u = q.front();
+            if (u->name == name)
+                return u->Turn;
+            q.pop();
+            if (visited[u->left] == 0 && u->left)
+            {
+                visited[u->left] = 1;
+                q.push(u->left);
+            }
+            if (visited[u->right] == 0 && u->right)
+            {
+                visited[u->right] = 1;
+                q.push(u->right);
+            }
+        }
+        //node not found
+        return -1;
+    }
+    Node* deleteNodeByName(string name)
+    {
+        //getting name of the guest and finding its turn to remove it
+        for (auto i : visited)
+            visited[i.first] = 0;
+        ll turn = searchByName(name);
+        if (turn != -1)
+            return deleteNodeByTurn(root, turn);
+        return root;
+    }
+    Node* deleteNodeByTurn(Node* root, ll turn)
+    {
+        //remove a node by its turn
+        if (root == NULL)
+            return root;
+        if (turn < root->Turn)
+            root->left = deleteNodeByTurn(root->left, turn);
+        else if (turn > root->Turn)
+            root->right = deleteNodeByTurn(root->right, turn);
+        else
+        {
+            if ((root->left == NULL) || (root->right == NULL))
+            {
+                Node* temp = root->left ? root->left : root->right;
+                if (temp == NULL)
+                {
+                    temp = root;
+                    root = NULL;
+                }
+                else
+                    *root = *temp;
+                free(temp);
+            }
+            else
+            {
+                Node* temp = minValueNode(root->right);
+                root->Turn = temp->Turn;
+                root->name = temp->name;
+                root->right = deleteNodeByTurn(root->right, temp->Turn);
+            }
+        }
+        if (root == NULL)
+            return root;
+        root->height = 1 + max(height(root->left), height(root->right));
+        ll balance = getBalance(root);
+        if (balance > 1 && getBalance(root->left) >= 0)
+            return rightRotate(root);
+        if (balance > 1 && getBalance(root->left) < 0)
+        {
+            root->left = leftRotate(root->left);
+            return rightRotate(root);
+        }
+        if (balance < -1 && getBalance(root->right) <= 0)
+            return leftRotate(root);
+        if (balance < -1 && getBalance(root->right) > 0)
+        {
+            root->right = rightRotate(root->right);
+            return leftRotate(root);
+        }
+        return root;
+    }
+    bool searchByTurn(Node* root, ll TurntoSearch)
+    {
+        //finding the node by getting its turn
+        if (root != NULL)
+        {
+            if (root->Turn == TurntoSearch)
+            {
+                cout << root->name << endl;
+                return true;
+            }
+            else if (root->Turn < TurntoSearch)
+            {
+                searchByTurn(root->right, TurntoSearch);
+            }
+            else
+            {
+                searchByTurn(root->left, TurntoSearch);
+            }
+        }
+        //node not found
+        return false;
+    }
+    void ShowTree()
+    {
+        //print the AVL tree
+        //vector to save every line
+        vector<vector<string>> lines;
+        vector<Node*> level;
+        //vector to save next line
+        vector<Node*> next;
+        level.push_back(root);
+        int c = 1;
+        //length of widest node name
+        int widest = 0;
+        while (c != 0)
+        {
+            vector<string> line;
+            c = 0;
+            for (auto n : level)
+            {
+                if (n == NULL)
+                {
+                    line.push_back("");
+                    next.push_back(NULL);
+                    next.push_back(NULL);
+                }
+                else
+                {
+                    string nodenam = n->name;
+                    line.push_back(nodenam);
+                    if (nodenam.length() > widest)
+                        widest = nodenam.length();
+                    next.push_back(n->left);
+                    next.push_back(n->right);
+                    if (n->left != NULL)
+                        c++;
+                    if (n->right != NULL)
+                        c++;
+                }
+            }
+            if (widest % 2 == 1)
+                widest++;
+            lines.push_back(line);
+            vector<Node*> tmp = level;
+            level = next;
+            next = tmp;
+            next.clear();
+        }
+        //number of spaces
+        int perpiece = lines.rbegin()->size() * (widest + 4);
+        for (int i = 0; i < lines.size(); i++)
+        {
+            vector<string> line = lines[i];
+            int hpw = (int)(perpiece / 2) - 1;
+            if (i > 0) {
+                for (int j = 0; j < line.size(); j++)
+                {
+                    string c = " ";
+                    if (j % 2 == 1)
+                    {
+                        if (line[j - 1] != "")
+                            c = "+";
+                        else if (line[j] != "")
+                        {
+                            for (int k = 0; k < lines[i - 1].size(); k++)
+                                cout << " ";
+                            c = "+";
+                        }
+                    }
+                    cout << c;
+                    if (line[j] == "")
+                        for (int k = 0; k < perpiece - 1; k++)
+                            cout << " ";
+                    else
+                    {
 
+                        for (int k = 0; k < hpw; k++)
+                            cout << (j % 2 == 0 ? " " : "-");
+                        cout << (j % 2 == 0 ? "| " : " |");
+                        for (int k = 0; k < hpw; k++)
+                            cout << (j % 2 == 0 ? "-" : " ");
+                    }
+                }
+                cout << endl;
+            }
+            for (int j = 0; j < line.size(); j++)
+            {
+                string f = line[j];
+                int gap1 = perpiece / 2 - f.size() / 2 + 1;
+                int gap2 = ceil(perpiece / 2.0) - ceil(f.size() / 2.0);
+                for (int k = 0; k < gap1; k++)
+                    cout << " ";
+                cout << f;
+                for (int k = 0; k < gap2; k++)
+                    cout << " ";
+            }
+            cout << endl;
+            perpiece /= 2;
+        }
+    }
+};
+bool isNumber(const string& str)
+{
+    //check str is number or not
+    return str.find_first_not_of("0123456789") == string::npos;
+}
+void Party()
+{
+    AVLTree AVL;
+    string Operation;
+    while (cin >> Operation)
+    {
+        if (Operation == "Insert")
+        {
+            string name;
+            ll turn;
+            cin >> name >> turn;
+            AVL.root = AVL.insert(AVL.root, turn, name);
+        }
+        else if (Operation == "Delete")
+        {
+            string TurnName;
+            cin >> TurnName;
+            if (isNumber(TurnName))
+            {
+                AVL.root = AVL.deleteNodeByTurn(AVL.root, stoi(TurnName));
+            }
+            else
+                AVL.root = AVL.deleteNodeByName(TurnName);
+        }
+        else if (Operation == "Search")
+        {
+            ll turn;
+            cin >> turn;
+            if (!AVL.searchByTurn(AVL.root, turn))
+                cout << "Not Found!" << endl;
+        }
+        else if (Operation == "ShowTree")
+        {
+            AVL.ShowTree();
+        }
+    }
+}
 int main(){
     string customer_detail;
     // getting constome's data
@@ -185,6 +528,36 @@ int main(){
             ss>>word; x.prepartion_time=stoi(word);
             ss>>word; x.eating_time=stoi(word);
             costomer_info.push_back(x);
+    }
+    //Party
+    AVLTree AVL;
+    string Operation;
+    while (cin >> Operation)
+    {
+        if (Operation == "Insert")
+        {
+            string name;
+            ll turn;
+            cin >> name >> turn;
+            AVL.root = AVL.insert(AVL.root, turn, name);
+        }
+        else if (Operation == "Delete")
+        {
+            string TurnName;
+            cin >> TurnName;
+            if (isNumber(TurnName))
+                AVL.root = AVL.deleteNodeByTurn(AVL.root, stoi(TurnName));
+            else AVL.root = AVL.deleteNodeByName(TurnName);
+        }
+        else if (Operation == "Search")
+        {
+            ll turn;
+            cin >> turn;
+            if (!AVL.searchByTurn(AVL.root, turn))
+                cout << "Not Found!" << endl;
+        }
+        else if (Operation == "ShowTree")
+            AVL.ShowTree();
     }
     return 0;
 }
